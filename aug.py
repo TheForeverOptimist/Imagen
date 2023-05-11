682c52485a2d7079f63e7dff9d935bf21dcd4e45
TheForeverOptimist <sean@elevate.app>
2023-05-11 11:06:07 -0400

    Load Images, Image Augmentation, Show Images

--- aug.py
+++ aug.py
@@ -4,10 +4,24 @@ import glob
 
 
 # 1.) Load Dataset
+images = []
 images_path = glob.glob("images/*.jpeg")
 for img_path in images_path:
     img = cv2.imread(img_path)
+    images.append(img)
 
+#2.) Image Augmentation
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+augmentation = iaa.Sequential([
+    #1. Rotate
+    # iaa.Rotate((-30, 30))
+    #2. Flip
+    iaa.Fliplr(1)
+])
+
+augmentated_images = augmentation(images=images)
+
+#3.) show images
+for img in augmentated_images:
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
