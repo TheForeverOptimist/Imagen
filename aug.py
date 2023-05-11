69319bfe3ae75f41998166cab25fa96cdbdc41ea
TheForeverOptimist <sean@elevate.app>
2023-05-11 11:35:10 -0400

    creating new images

--- aug.py
+++ aug.py
@@ -16,12 +16,35 @@ augmentation = iaa.Sequential([
     #1. Rotate
     # iaa.Rotate((-30, 30))
     #2. Flip
-    iaa.Fliplr(1)
+    iaa.Fliplr(0.5),
+    iaa.Flipud(0.5),
+
+    #3. Affine
+    iaa.Affine(translate_percent={"x": (-0.5, 0.5), "y": (-0.5, 0.5)},
+               rotate=(-30, 30),
+               scale=(0.5, 1.5),
+               ),
+    #4. Multiply -- which makes the image brighter or darker
+    iaa.Multiply((0.8, 1.2)),
+
+    #5 Linear Contrast
+    iaa.LinearContrast((0.6, 1.4)),
+
+    #Perform the below messages only sometime
+    iaa.Sometimes(0.5, 
+        #6. gaussian blur
+        iaa.GaussianBlur((0.0, 3.0)),
+                  
+                  )
+
+
+    
+
 ])
 
-augmentated_images = augmentation(images=images)
-
 #3.) show images
-for img in augmentated_images:
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
+while True:
+    augmented_images = augmentation(images=images)
+    for img in augmented_images:
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
