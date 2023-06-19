eec1af98e44eb5c68e56be0444d2edfa6699207f
TheForeverOptimist <sean@elevate.app>
2023-06-19 13:36:16 -0400

    learning how to dynamically select a function

--- functions.py
+++ functions.py
@@ -4,8 +4,24 @@
 # print(fahr_to_celsius(50))
 
 
-def first_function():
-  pass
+# def first_function():
+#   pass
 
-result = first_function()
-print(result)
+# result = first_function()
+# print(result)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+math_operations = {
+    '+': add,
+    '-': subtract
+}
+
+selected_operation = '+'
+
+print(math_operations[selected_operation](2,4))
+
