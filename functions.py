e404f743c10d3c5fe7b1908d31d1c9ff9f6de43b
TheForeverOptimist <sean@elevate.app>
2023-06-18 23:04:56 -0400

    learning funcitons

--- functions.py
+++ functions.py
@@ -1,4 +1,11 @@
-def fahr_to_celsius(temp):
-    return((temp - 32) * (5/9))
+# def fahr_to_celsius(temp):
+#     return((temp - 32) * (5/9))
 
-print(fahr_to_celsius(50))
+# print(fahr_to_celsius(50))
+
+
+def first_function():
+  pass
+
+result = first_function()
+print(result)
