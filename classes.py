3f6c7afd507f3a50ac7af6b0ddf64031eb6eec4f
TheForeverOptimist <sean@elevate.app>
2023-06-24 18:23:10 -0400

    added bankaccount class

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import random
+
 # class Dog():
 #     def __init__(self, name, age=0):
 #         self.name = name
@@ -26,30 +28,71 @@
 
 # print(lassie.name, lassie.age)
 
-class Vehicle():
-    def __init__(self, make, model, running=False):
-        self.make = make
-        self.model = model
-        self.running= running
+# class Vehicle():
+#     next_id = 1
 
-    def start(self):
-        running = True
-        print('running...')
+#     def __init__(self, make, model):
+#         self.make = make
+#         self.model = model
+#         self.running= False
+#         self.id = Vehicle.next_id
+#         Vehicle.next_id += 1
 
-    def stop(self):
-        running = False
-        print('stopped...')
+#     def start(self):
+#         running = True
+#         print('running...')
 
-    def __str__(self):
-        return f'Vehicle is a {self.make} model {self.model}'
+#     def stop(self):
+#         running = False
+#         print('stopped...')
+
+#     def __str__(self):
+#         return f'Vehicle is a {self.make} model {self.model}'
+    
+#     @classmethod
+#     def get_total_cars(cls):
+#         return cls.next_id - 1
     
 
-car = Vehicle('Audi', 'Q7')
+# car1 = Vehicle('Audi', 'Q7')
+# car2 = Vehicle('Toyota', 'Camry')
+# car3 = Vehicle('Chevy', 'Cruze')
 
-print(car)
+# #call the class method to get the total number of cars
 
-print(car.make, car.model)
+# # total_cars = Vehicle.get_total_cars()
+# print(f'Total number of cars is {Vehicle.get_total_cars()}')
 
-car.start()
+# print(car)
 
-car.stop()
+# print(car.make, car.model)
+
+# car.start()
+
+# car.stop()
+
+# print(car.id)
+
+class BankAccount():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+        self.account_no = random.randint(111111, 999999)
+
+    def deposit(amount):
+        balance += amount
+
+    def withdraw(amount):
+        balance -= amount
+
+    def __str__(self):
+        return f'The Bank {self.account_no} is owned by {self.owner} has a balance of {self.balance}'
+    
+chase = BankAccount('John Michael', 40000)
+
+print(chase)
+
+print(chase.owner, chase.balance, chase.account_no)
+
+
+
